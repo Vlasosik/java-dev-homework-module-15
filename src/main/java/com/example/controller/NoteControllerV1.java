93189703430470867117336ad73e4e02aad17be1
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-02-24 17:44:48 +0200

    cookies added

--- src/main/java/com/example/controller/NoteControllerV1.java
+++ src/main/java/com/example/controller/NoteControllerV1.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import com.example.entity.Note;
 import com.example.exception.NoteNotFoundException;
 import com.example.service.NoteService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,30 +15,38 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
+
 @Validated
 @Controller
 @RequestMapping("/note")
-public class NoteController {
+public class NoteControllerV1 {
     private static final String PATH_NOTE_HTML_FILE = "note_configure/note";
     private static final String PATH_NOTE_EDIT_HTML_FILE = "note_configure/note_edit";
+    private static final String REDIRECT_NOTE_LIST = "redirect:/note/list";
+    private static final String THEME = "theme";
     private final NoteService noteService;
 
     @Autowired
-    public NoteController(NoteService noteService) {
+    public NoteControllerV1(NoteService noteService) {
         this.noteService = noteService;
     }
 
     @GetMapping("/list")
-    public ModelAndView getNoteAll() {
+    public ModelAndView getNoteAll(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView result = new ModelAndView(PATH_NOTE_HTML_FILE);
+        String theme = getThemeCookie(request);
+        if (theme == null || !theme.equals("black")) {
+            setThemeCookie(response, "white");
+            theme = "white";
+        }
         Map<Long, Note> note = noteService.mapAll();
-        return result.addObject("noteAll", note.values());
+        return result.addObject("noteAll", note.values()).addObject(THEME, theme);
     }
 
     @PostMapping("/delete")
     public String deleteNoteById(@Valid @NotNull @RequestParam("id") Long id) throws NoteNotFoundException {
         noteService.deleteById(id);
-        return "redirect:/note/list";
+        return REDIRECT_NOTE_LIST;
     }
 
     @GetMapping("/edit")
@@ -48,6 +59,28 @@ public class NoteController {
     @PostMapping("/edit")
     public String updateNote(@Valid @NotNull @ModelAttribute("note") Note note) throws NoteNotFoundException {
         noteService.update(note);
-        return "redirect:/note/list";
+        return REDIRECT_NOTE_LIST;
+    }
+    @PostMapping("/changeTheme")
+    public String changeTheme(HttpServletResponse response, @RequestParam(THEME) String theme){
+        setThemeCookie(response, theme);
+        return REDIRECT_NOTE_LIST;
+    }
+
+    private void setThemeCookie(HttpServletResponse response, String theme) {
+        Cookie themeCookie = new Cookie(THEME, theme);
+        response.addCookie(themeCookie);
+    }
+
+    private String getThemeCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(THEME)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
